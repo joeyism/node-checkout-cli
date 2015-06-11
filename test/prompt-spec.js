fbd850b5dae6a6f322b1f96122919d5f2c7f9d03
joeyism <sham.joey@gmail.com>
2015-06-11 18:32:42 -0400

    Updated unit test

---
+++ test/prompt-spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var mockery = require('mockery');
+var expect = require('chai').expect;
+var branches = ['branch1', 'branch2'];
+var prompt;
+
+describe('prompt', function(){
+
+    describe('branches', function(){
+
+        beforeEach(function(done){
+            mockery.enable({
+                warnOnReplace: false,
+                warnOnUnregistered: false,
+                useCleanCache: true
+            });
+            done();
+        });
+
+        afterEach(function(done){
+            mockery.resetCache();
+            mockery.deregisterAll();
+            done();
+        });
+
+        it('should successfully return the answers to the user\' prompt questions',function(done){
+            var answer = { checkoutBranch: branches[0] }; 
+            var mockInq = {
+                prompt: function(question, callback){
+                    callback(answer);
+                }
+            };
+            mockery.registerMock('inquirer', mockInq);
+            prompt = require('../lib/prompt');
+            prompt.branches(branches).then(function(result){
+                expect(result).to.equal(branches[0]);
+                done();
+            });
+        });
+    });
+});
